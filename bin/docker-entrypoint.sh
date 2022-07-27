a0919425dfb4dc970a4ef55dfc1d7906bcf4bf36
rymsha <rymsha@yandex.ru>
2022-07-27 15:37:37 +0200

    Improve setup_xp_home_directory #23
    (cherry picked from commit db19ac6179cc24a84cb7fa2916d28acb21607a4e)

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -36,7 +36,6 @@ setup_xp_home_directory() {
 	# Is there a custom setenv.sh ?
 	if [[ -f $XP_HOME/setenv.sh ]]; then
 		echo "Found custom setenv.sh file in XP_HOME folder, copying it into runtime..."
-		rm -f $XP_ROOT/bin/setenv.sh
 		cp -p $XP_HOME/setenv.sh $XP_ROOT/bin/setenv.sh
 	fi
 }
